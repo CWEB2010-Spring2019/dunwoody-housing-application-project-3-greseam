e4e68390b9704ebccc468e21fb806fb71c089a32
greseam <greseam@CND82405RT>
2019-03-23 23:18:27 -0500

    Implemented Search and fixed json issues

--- Project_Three_GUI/newResident.xaml.cs
+++ Project_Three_GUI/newResident.xaml.cs
@@ -23,10 +23,11 @@ namespace Project_Three_GUI
     /// </summary>
     public partial class newResident : Page
     {
+        List<Residents> residents = new List<Residents>();
         public newResident()
         {
             InitializeComponent();
-            List<Residents> residents = new List<Residents>();
+            
             residents =  LoadCollectionData();
             HousingGrid.ItemsSource = residents;
             var studentTypeScholarship = from resident in residents
@@ -46,11 +47,67 @@ namespace Project_Three_GUI
             var studentTypeAthletes = from resident in residents
                 where resident.StudentType == "Athlete"
                 select resident;
-            foreach (var VARIABLE in studentTypeWorkers)
+            foreach (var VARIABLE in studentTypeAthletes)
             {
                 NumOfAtheletes.Content = Convert.ToInt32(NumOfAtheletes.Content.ToString()) + 1;
             }
 
+            var ResidentOnFL1 = from resident in residents
+                where resident.FloorNum == 1
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL1)
+            {
+                NumOfResidentsFl1.Content = "FL1 : " + Convert.ToInt32(ResidentOnFL1.Count());
+            }
+            var ResidentOnFL2 = from resident in residents
+                where resident.FloorNum == 2
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL2)
+            {
+                NumOfResidentsFl2.Content = "FL2 : " + Convert.ToInt32(ResidentOnFL2.Count());
+            }
+            var ResidentOnFL3 = from resident in residents
+                where resident.FloorNum == 3
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL3)
+            {
+                NumOfResidentsFl3.Content = "FL3 : " + Convert.ToInt32(ResidentOnFL3.Count());
+            }
+            var ResidentOnFL4 = from resident in residents
+                where resident.FloorNum == 4
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL4)
+            {
+                NumOfResidentsFl4.Content = "FL4 : " + Convert.ToInt32(ResidentOnFL4.Count());
+            }
+            var ResidentOnFL5 = from resident in residents
+                where resident.FloorNum == 5
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL5)
+            {
+                NumOfResidentsFl5.Content = "FL5 : " + Convert.ToInt32(ResidentOnFL5.Count());
+            }
+            var ResidentOnFL6 = from resident in residents
+                where resident.FloorNum == 6
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL6)
+            {
+                NumOfResidentsFl6.Content = "FL6 : " + Convert.ToInt32(ResidentOnFL6.Count());
+            }
+            var ResidentOnFL7 = from resident in residents
+                where resident.FloorNum == 7
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL7)
+            {
+                NumOfResidentsFl7.Content = "FL7 : " + Convert.ToInt32(ResidentOnFL7.Count());
+            }
+            var ResidentOnFL8 = from resident in residents
+                where resident.FloorNum == 8
+                select resident;
+            foreach (var VARIABLE in ResidentOnFL8)
+            {
+                NumOfResidentsFl1.Content = "FL8 : " + Convert.ToInt32(ResidentOnFL8.Count());
+            }
         }
 
         private List<Residents> LoadCollectionData()
@@ -58,7 +115,7 @@ namespace Project_Three_GUI
             string FilePath = @"AddedResident.json";
             List<Residents> addedResidentsList = new List<Residents>();
             addedResidentsList =
-                JsonConvert.DeserializeObject<List<Residents>>(File.ReadAllText(FilePath));
+                JsonConvert.DeserializeObject<List<Residents>>(File.ReadAllText(FilePath).Replace("][",","));
             List<Residents> ResidentsDataList = new List<Residents>();
             ResidentsDataList.Add(new Residents()
             {
@@ -66,7 +123,7 @@ namespace Project_Three_GUI
                 Name = "Larry L.",
                 StudentType = "Athlete",
                 FloorNum = 4,
-                RoomNum = 2,
+                RoomNum = 1,
                 Rent = 1200,
             });
 
@@ -76,7 +133,7 @@ namespace Project_Three_GUI
                 Name = "Mike Gold",
                 StudentType = "Scholarship",
                 FloorNum = 7,
-                RoomNum = 5,
+                RoomNum = 1,
                 Rent = 100,
             });
 
@@ -86,7 +143,7 @@ namespace Project_Three_GUI
                 Name = "Mathew Mannen",
                 StudentType = "WorkStudent",
                 FloorNum = 3,
-                RoomNum = 4,
+                RoomNum = 1,
                 Rent = 1245 - ((14*(20 * 3))/2),
             });
             
@@ -118,5 +175,49 @@ namespace Project_Three_GUI
             
         }
 
+        private void IDSearch_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var IDSearchQuery = from resident in residents
+                    where resident.ID == Convert.ToInt32(SearchTextBox.Text)
+                    select resident;
+
+                    HousingGrid.ItemsSource = IDSearchQuery;
+
+            }
+            catch 
+            {
+                SearchTextBox.Text = "Invalid ID";
+            }
+        }
+
+        private void FloorSearch_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var FloorSearchQuery = from resident in residents
+                    where resident.FloorNum == Convert.ToInt32(SearchTextBox.Text)
+                    select resident;
+
+                HousingGrid.ItemsSource = FloorSearchQuery;
+
+            }
+            catch
+            {
+                SearchTextBox.Text = "Invalid Floor";
+            }
+        }
+
+        private void Search_Clear_OnClick(object sender, RoutedEventArgs e)
+        {
+            HousingGrid.ItemsSource = residents;
+        }
+
+
+        private void SearchTextBox_OnGotMouseCapture(object sender, MouseEventArgs e)
+        {
+            SearchTextBox.Text = " ";
+        }
     }
 }
--- Project_Three_GUI/selection.xaml.cs
+++ Project_Three_GUI/selection.xaml.cs
@@ -32,70 +32,123 @@ namespace Project_Three_GUI
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-        
+
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             frame3.Content = new newResident();
         }
+
         newResident page3 = new newResident();
         JArray canContainer = new JArray();
+        private int selectedData = 0;
+        private int floorVal = 0;
+        string[,] Floors =
+        {
+            {"1","2","3"},
+            {"4","5","6"},
+            {"7", "8",""}
+        };
+        private int roomNumVal = 1;
         private void ButtonBase_Enter(object sender, RoutedEventArgs e)
         {
 
-            if (StudentFirstName.ToString().Length >= 1 && StudentSurName.ToString().Length >= 1 && IDnum.ToString().Length >= 1 && (ScholarshipBox.IsChecked == true ^ AthleteBox.IsChecked == true ^ WorkerBox.IsChecked == true))
+            if (StudentFirstName.ToString().Length >= 1 && StudentSurName.ToString().Length >= 1 &&
+                IDnum.ToString().Length >= 1 && (ScholarshipBox.IsChecked == true ^ AthleteBox.IsChecked == true ^
+                                                 WorkerBox.IsChecked == true))
             {
-       
-            string FilePath = @"AddedResident.json";
-            int Rent = 0;
-            string type = "";
-            int ID = Convert.ToInt16(IDnum.Text);
-            string fullName = StudentFirstName.Text.ToString() + " " + StudentSurName.Text.ToString();
-            try
-            {
-                int hours = Convert.ToInt32(Hourly.Text);
-                int workingWage = 14 * hours;
+
+                if (ScholarshipBox.IsChecked == true)
+                {
+                    if (floorText.Text.ToString() == Floors[2,0] || floorText.Text.ToString() == Floors[2,1])
+                    {
+                        floorVal = Convert.ToInt32(floorText.Text.ToString());
+                        roomNumVal++;
+                    }
+                    else
+                    {
+
+                    }
+                }
                 if (WorkerBox.IsChecked == true)
                 {
-                    Rent = 1245 - (workingWage / 2);
-                    type = "WorkStudent";
-                }
-            }
-            catch (Exception exception)
-            {
-               
-            }
-            Residents addResidents = new Residents();
-            if (ScholarshipBox.IsChecked == true)
-            {
-                Rent = 100;
-                type = "Scholarship";
-            }
-            else if (AthleteBox.IsChecked == true)
-            {
-                Rent = 1200;
-                type = "Athlete";
-            }
-            else 
-            {
-                
-            }
-            
-            TextWriter tsw = new StreamWriter(FilePath, true);
+                    if (floorText.Text.ToString() == Floors[0, 0] || floorText.Text.ToString() == Floors[0, 1] || floorText.Text.ToString() == Floors[0, 2])
+                    {
+                        floorVal = Convert.ToInt32(floorText.Text.ToString());
+                        roomNumVal++;
 
-             JObject addJObject = new JObject(new JProperty("ID", ID),
-                new JProperty("Name", fullName),
-                new JProperty("StudentType", type),
-                new JProperty("FloorNum", 1),
-                new JProperty("RoomNum", 6),
-                new JProperty("Rent", Rent));
-            canContainer.Add(addJObject);
-           
-            //string newData = "";
-            //FileStream inputFileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
-            //StreamReader reader = new StreamReader(inputFileStream);
-            //newData = Convert.ToString(File.ReadAllText(FilePath)).Replace("][", ",");
+                    }
+                    else
+                    {
+
+                    }
+                }
+                if (WorkerBox.IsChecked == true)
+                {
+                    if (floorText.Text.ToString() == Floors[1, 0] || floorText.Text.ToString() == Floors[1, 1] || floorText.Text.ToString() == Floors[1, 2])
+                    {
+                        floorVal = Convert.ToInt32(floorText.Text.ToString());
+                        roomNumVal++;
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+                string FilePath = @"AddedResident.json";
+                int Rent = 0;
+                string type = "";
+                int ID = Convert.ToInt16(IDnum.Text);
+                string fullName = StudentFirstName.Text.ToString() + " " + StudentSurName.Text.ToString();
+                try
+                {
+                    int hours = Convert.ToInt32(Hourly.Text);
+                    int workingWage = 14 * hours;
+                    if (WorkerBox.IsChecked == true)
+                    {
+                        Rent = 1245 - (workingWage / 2);
+                        type = "WorkStudent";
+                    }
+                }
+                catch (Exception exception)
+                {
+
+                }
+
+                Residents addResidents = new Residents();
+                if (ScholarshipBox.IsChecked == true)
+                {
+                    Rent = 100;
+                    type = "Scholarship";
+                }
+                else if (AthleteBox.IsChecked == true)
+                {
+                    Rent = 1200;
+                    type = "Athlete";
+                }
+                else
+                {
+
+                }
+
+                TextWriter tsw = new StreamWriter(FilePath, true);
+               
+                JObject addJObject = new JObject(new JProperty("ID", ID),
+                    new JProperty("Name", fullName),
+                    new JProperty("StudentType", type),
+                    new JProperty("FloorNum", 1),
+                    new JProperty("RoomNum", roomNumVal),
+                    new JProperty("Rent", Rent));
+
+
+                canContainer.Add(addJObject);
+
+                //string newData = "";
+                //FileStream inputFileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+                //StreamReader reader = new StreamReader(inputFileStream);
+                //newData = Convert.ToString(File.ReadAllText(FilePath)).Replace("][", ",");
                 //  File.AppendAllText(FilePath, addJObject.ToString());
 
 
@@ -106,9 +159,10 @@ namespace Project_Three_GUI
                     writer.Close();
                     tsw.Close();
                 }
+
                 try
                 {
-                    
+
 
                 }
                 catch (Exception exception)
@@ -116,21 +170,23 @@ namespace Project_Three_GUI
                     Console.WriteLine(exception);
                     throw;
                 }
+
                 //add Rent to JSON file
                 //return StudentName, id, and type to Json to be saved
-                frame3.Navigate(page3);
-        
-       
+                frame3.Content = new newResident();
+
 
 
             }
         }
 
-        private void ScholarshipBox_OnChecked(object sender, RoutedEventArgs e)
+ 
+    private void ScholarshipBox_OnChecked(object sender, RoutedEventArgs e)
         {
 
             AthleteBox.Visibility = Visibility.Hidden;
             WorkerBox.Visibility = Visibility.Hidden;
+            
         }
 
         private void AthleteBox_OnChecked(object sender, RoutedEventArgs e)
@@ -138,6 +194,7 @@ namespace Project_Three_GUI
 
             WorkerBox.Visibility = Visibility.Hidden;
             ScholarshipBox.Visibility = Visibility.Hidden;
+
         }
 
         private void WorkerBox_OnChecked(object sender, RoutedEventArgs e)
@@ -147,27 +204,48 @@ namespace Project_Three_GUI
             AthleteBox.Visibility = Visibility.Hidden;
             Hourly.Visibility = Visibility.Visible;
             hourlyDescription.Visibility = Visibility.Visible;
+          
 
         }
 
         private void ScholarshipBox_OnUnchecked(object sender, RoutedEventArgs e)
         {
+            
             AthleteBox.Visibility = Visibility.Visible;
             WorkerBox.Visibility = Visibility.Visible;
         }
 
         private void AthleteBox_OnUnchecked(object sender, RoutedEventArgs e)
         {
+          
             WorkerBox.Visibility = Visibility.Visible;
             ScholarshipBox.Visibility = Visibility.Visible;
         }
 
         private void WorkerBox_OnUnchecked(object sender, RoutedEventArgs e)
         {
+           
             ScholarshipBox.Visibility = Visibility.Visible;
             AthleteBox.Visibility = Visibility.Visible;
             Hourly.Visibility = Visibility.Hidden;
             hourlyDescription.Visibility = Visibility.Hidden;
+        }
+
+        private void UIElement_OnDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+
+        private void FloorText_OnDropDownOpened(object sender, EventArgs e)
+        {
+            floorText.Text = Floors[0,1];
+            selectedData = Convert.ToInt32(floorText.Text);
+        }
+
+        private void FloorText_OnDropDownClosed(object sender, EventArgs e)
+        {
+            floorText.Text = selectedData.ToString();
         }
     }
 }
